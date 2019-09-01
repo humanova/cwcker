f16ee5c82122f6e5c91e36cc3e633c081c0a93a9
humanova <craxes_humanova@hotmail.com>
2019-09-01 21:36:54 +0300

    add codecs module comparing html string w/ utf-8 format

--- src/checker.py
+++ src/checker.py
@@ -1,16 +1,17 @@
 # 2019 Emir Erbasan (humanova)
 
 import requests
+import codecs
 
 def check_cw_website():
     r = requests.get("https://cubeworld.com")
     
     curr_html = r.text
-    prev_html = open("website.txt", "r").read()
+    prev_html = codecs.open("website.txt", "r", "utf-8").read()
 
     if not len(curr_html) == len(prev_html):
         change_text = f"Change detected!\n\nOld/New char count : {len(prev_html)} - {len(curr_html)}\nNew html :\n```{curr_html}```"
-        open("website.txt", "w+").write(curr_html)
+        codecs.open("website.txt", "w+", "utf-8").write(curr_html)
 
         return (True, change_text)
 
